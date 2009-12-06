ad2305e1b7e9afadccfa2935354c0662783e03d6
xwisdom <xwisdom@a74905c6-12a6-11de-a799-eb79a21f649b>
2009-12-06 13:19:15 +0000

    Branch to create 1.0 Beta 3

---
+++ branches/1.0.beta3/pdi-examples/form-state.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once "../raxan/pdi/gateway.php";
+
+class WebForm extends RichWebPage {
+
+    protected function _init() {
+        $this->source('views/webform.html');
+        $this->loadCSS('master');
+
+        // update form values on postback
+        $this->preserveFormContent = true;
+    }
+    
+}
+
+RichWebPage::Init('WebForm')
+
+?>
---
+++ branches/1.0.beta3/raxan/templates/page-code-behind.js
@@ -0,0 +1,16 @@
+/**
+ *  Sample JavaScript code behind file
+ *
+ */
+
+// include other js plugins or style sheets
+// html.css('master'); // master style sheet
+// html.include('something.js');
+
+// html ready event
+html.ready(function() {
+
+    // put your code here
+
+})
+
